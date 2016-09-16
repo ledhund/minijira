8946ba54439963754001d99bd6d7fca26a1744a8
Anders Hedlund <anders.hedlund@cybercom.com>
2016-09-16 14:52:29 +0200

    better selector

--- wipe.js
+++ wipe.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-    code: 'document.querySelectorAll(\'li.ghx-column[data-column-id="4190"],li.ghx-column[data-id="4190"],li.ghx-column[data-column-id="3523"],li.ghx-column[data-id="3523"],li.ghx-column[data-column-id="3507"],li.ghx-column[data-id="3507"]\').forEach(function(el) {el.style.display = "none";})'
+    code: 'li.ghx-column:nth-last-child(3).forEach(function(el) {el.style.display = "none";})'
   });
 });
