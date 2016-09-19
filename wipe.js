f262abada2cadc0299c394dc272c336fd04d16b8
Anders Hedlund <anders.hedlund@cybercom.com>
2016-09-19 17:27:16 +0200

    inserting css instead

--- wipe.js
+++ wipe.js
@@ -1,3 +1,7 @@
 chrome.browserAction.onClicked.addListener(function () {
-   chrome.tabs.executeScript(null, {file: "remove.js"});
+   var css = 'li.ghx-column:nth-last-child(-n+{{nums}}) { display: none; }';
+   chrome.storage.sync.get('nums', function (stored) {
+      css = css.replace('{{nums}}', stored.nums);
+      chrome.tabs.insertCSS(null, {code: css});
+   });
 });
