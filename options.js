c8f1395ca06bd66e84c7ff325a3ba2c1839c807b
=Anders Hedlund <anders@hedlund.de>
2016-09-24 20:43:02 +0200

    running css on whitelisted hosts on pageload!

--- options.js
+++ options.js
@@ -4,44 +4,41 @@ var hostList = document.getElementById('added-hosts');
 
 var hosts;
 
-
 function addHost () {
-	var host = newHostInput.value;
-	host = host.replace(/^\s*/,'').replace(/\s*$/,'');
-	hosts.push(host);
-	renderHosts();
-	newHostInput.value = '';
-
+  var host = newHostInput.value;
+  host = host.replace(/^\s*/,'').replace(/\s*$/,'');
+  hosts.push(host);
+  renderHosts();
+  newHostInput.value = '';
 }
 
 function renderHosts() {
-	hostList.innerHTML = hosts.map(function (host) {
-		return '<option value="' + host + '">' + host + '</option>'
-	}).join('');
-
+  hostList.innerHTML = hosts.map(function (host) {
+    return '<option value="' + host + '">' + host + '</option>'
+  }).join('');
 }
 
 function write() {
-	var nums = colsToRemoveInput.value.replace(/\D/,'');
-	if (!isNaN(nums)) {
-		chrome.storage.sync.set({
-			nums: nums,
-			hosts: hosts
-		}, function () {
-         document.getElementById('status').innerHTML = 'Saved!';
-         setTimeout(function () {
-            document.getElementById('status').innerHTML = '';
-         }, 800);
-		});
-	}
+  var nums = colsToRemoveInput.value.replace(/\D/,'');
+  if (!isNaN(nums)) {
+    chrome.storage.sync.set({
+      nums: nums,
+      hosts: hosts
+    }, function () {
+      document.getElementById('status').innerHTML = 'Saved!';
+      setTimeout(function () {
+        document.getElementById('status').innerHTML = '';
+      }, 800);
+    });
+  }
 }
 
 function read() {
-	chrome.storage.sync.get({nums: 1, hosts: []}, function (stored) {
-		colsToRemoveInput.value = stored.nums;
-		hosts = stored.hosts;
-		renderHosts();
-	});
+  chrome.storage.sync.get({nums: 1, hosts: []}, function (stored) {
+    colsToRemoveInput.value = stored.nums;
+    hosts = stored.hosts;
+    renderHosts();
+  });
 }
 
 document.getElementById('save').addEventListener('click', write);
--- wipe.js
+++ wipe.js
@@ -1,7 +1,16 @@
-chrome.browserAction.onClicked.addListener(function () {
-   var css = 'li.ghx-column:nth-last-child(-n+{{nums}}) { display: none; }';
-   chrome.storage.sync.get('nums', function (stored) {
+function matches(url, hostlist) {
+  var parser = document.createElement('a');
+  parser.href = url;
+  return hostlist.indexOf(parser.hostname) !== -1;
+}
+
+var css = 'li.ghx-column:nth-last-child(-n+{{nums}}) { display: none; }';
+
+chrome.webNavigation.onCompleted.addListener(function (details) {
+  chrome.storage.sync.get({nums: 1, hosts: []}, function (stored) {
+   if (matches(details.url, stored.hosts)) {
       css = css.replace('{{nums}}', stored.nums);
-      chrome.tabs.insertCSS(null, {code: css});
-   });
+      chrome.tabs.insertCSS(details.tabId, {code: css});
+    }
+  });
 });
