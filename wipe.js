e8fe94f5dc90d87ebe6d4ee8e0da892b56fa764d
Anders Hedlund <anders.hedlund@cybercom.com>
2016-09-26 09:33:18 +0200

    clicking the button unhides the removed stuff

--- wipe.js
+++ wipe.js
@@ -4,13 +4,20 @@ function matches(url, hostlist) {
   return hostlist.indexOf(parser.hostname) !== -1;
 }
 
-var css = 'li.ghx-column:nth-last-child(-n+{{nums}}) { display: none; }';
+var css = 'li.ghx-column:nth-last-child(-n+{{nums}}) { display: {{display}}; }';
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.storage.sync.get({nums: 1, hosts: []}, function (stored) {
+    var newCss = css.replace('{{nums}}', stored.nums).replace('{{display}}', 'table-cell');
+    chrome.tabs.insertCSS(tab.id, {code: newCss});
+  });
+});
 
 chrome.webNavigation.onCompleted.addListener(function (details) {
   chrome.storage.sync.get({nums: 1, hosts: []}, function (stored) {
-   if (matches(details.url, stored.hosts)) {
-      css = css.replace('{{nums}}', stored.nums);
-      chrome.tabs.insertCSS(details.tabId, {code: css});
+    if (matches(details.url, stored.hosts)) {
+      var newCss = css.replace('{{nums}}', stored.nums).replace('{{display}}', 'none');
+      chrome.tabs.insertCSS(details.tabId, {code: newCss});
     }
   });
 });
