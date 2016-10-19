4a6b57b9daed942f446e6c8da171e90ae978b401
Anders Hedlund <anders.hedlund@cybercom.com>
2016-10-19 13:03:57 +0200

    Hiding drop zones in the swim lanes too

--- wipe.js
+++ wipe.js
@@ -4,19 +4,26 @@ function matches(url, hostlist) {
   return hostlist.indexOf(parser.hostname) !== -1;
 }
 
-var css = 'li.ghx-column:nth-last-child(-n+{{nums}}) { display: {{display}}; }';
+var css = `
+li.ghx-column:nth-last-child(-n+{{nums}}) {
+   display: {{display}};
+}
+div.ghx-zone-overlay-column:nth-last-child(-n+{{nums}}) {
+   display: {{display}};
+}
+`
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.storage.sync.get({nums: 1, hosts: []}, function (stored) {
-    var newCss = css.replace('{{nums}}', stored.nums).replace('{{display}}', 'table-cell');
+  chrome.storage.sync.get({nums: 1, hosts: []}, function (options) {
+    var newCss = css.replace(/{{nums}}/g, options.nums).replace(/{{display}}/g, 'table-cell');
     chrome.tabs.insertCSS(tab.id, {code: newCss});
   });
 });
 
 chrome.webNavigation.onCompleted.addListener(function (details) {
-  chrome.storage.sync.get({nums: 1, hosts: []}, function (stored) {
-    if (matches(details.url, stored.hosts)) {
-      var newCss = css.replace('{{nums}}', stored.nums).replace('{{display}}', 'none');
+  chrome.storage.sync.get({nums: 1, hosts: []}, function (options) {
+    if (matches(details.url, options.hosts)) {
+      var newCss = css.replace(/{{nums}}/g, options.nums).replace(/{{display}}/g, 'none');
       chrome.tabs.insertCSS(details.tabId, {code: newCss});
     }
   });
