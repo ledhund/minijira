fb9e5d944a13c82466c05ff635d5cf3b03b20802
Anders Hedlund <anders.hedlund@cybercom.com>
2016-10-28 15:04:45 +0200

    button toggles, state saved in storage.sync

--- wipe.js
+++ wipe.js
@@ -13,17 +13,23 @@ div.ghx-zone-overlay-column:nth-last-child(-n+{{nums}}) {
 }
 `
 
+function saveState(display) {
+  chrome.storage.sync.set({display: display}, function () {});
+}
+
 chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.storage.sync.get({nums: 1, hosts: []}, function (options) {
-    var newCss = css.replace(/{{nums}}/g, options.nums).replace(/{{display}}/g, 'table-cell');
+  chrome.storage.sync.get({nums: 1, hosts: [], display: 'table-cell'}, function (options) {
+    var display = options.display === 'none' ? 'table-cell' : 'none';
+    var newCss = css.replace(/{{nums}}/g, options.nums).replace(/{{display}}/g, display);
     chrome.tabs.insertCSS(tab.id, {code: newCss});
+    saveState(display);
   });
 });
 
 chrome.webNavigation.onCompleted.addListener(function (details) {
-  chrome.storage.sync.get({nums: 1, hosts: []}, function (options) {
+  chrome.storage.sync.get({nums: 1, hosts: [], display: 'none'}, function (options) {
     if (matches(details.url, options.hosts)) {
-      var newCss = css.replace(/{{nums}}/g, options.nums).replace(/{{display}}/g, 'none');
+      var newCss = css.replace(/{{nums}}/g, options.nums).replace(/{{display}}/g, options.display);
       chrome.tabs.insertCSS(details.tabId, {code: newCss});
     }
   });
